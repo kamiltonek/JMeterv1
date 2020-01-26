301ef1fbc775f7e64752b73d4151647533f3164e
Kamil Tomczak <kamiltonek@wp.pl>
2020-01-26 16:20:38 +0100

    Fixed paths bug

--- scheduler.bat
+++ scheduler.bat
@@ -1,3 +1,3 @@
 C:
 cd C:\Programy\JMeter\apache-jmeter-5.1.1\bin
-java -jar ApacheJMeter.jar -n -t "D:\Studia\Semestr V\Podstawy Testowania\Zadanie1b\testPlanDB.jmx"
+java -jar ApacheJMeter.jar -n -t "D:\Studia\SemestrV\PodstawyTestowania\Zadanie1b\testPlanDB.jmx"
